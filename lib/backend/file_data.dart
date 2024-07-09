c3facee9a6ddc9f7261afebdd2fb8288cff4707e
Sem VB <sem.van.broekhoven@gmail.com>
2024-07-10 00:14:06 +0200

    added file data for readme file

--- lib/backend/file_data.dart
+++ lib/backend/file_data.dart
@@ -1,17 +1,23 @@
 import 'package:googleapis/drive/v3.dart' as drive;
 import 'package:keeper_of_projects/google_api/http_client.dart';
+import 'package:keeper_of_projects/backend/readme_content.dart' as readme;
 
 
 const String fileName = "__user-data.json";
-const String fileNameDefaultContent = "{}";
-const String settingsData = "__user-settings.json";
-const String settingsDataDefaultContent = "{darkmode = false}";
+const String fileDefaultContent = "{}";
+const String settingsName = "__user-settings.json";
+const String settingsDefaultContent = "{darkmode = false}";
 const String parentFolderName = "com.semerded";
 const String folderName = "keeper-of-projects";
 
+drive.File? appFolder;
 drive.File? userData;
-drive.File? userSettings;
+drive.File? settingsData;
 
 Map<String, String>? authHeaders;
 GoogleHttpClient? authenticateClient;
 drive.DriveApi? driveApi;
+
+//^ file data for readme file
+const String readmeName = "README.txt";
+const String readmeContent = readme.content;
