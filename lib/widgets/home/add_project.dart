a76639be0134acac028c092e318febb924b20cdb
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2024-07-17 00:10:42 +0200

    added a non functional add project button with an animation when no projects are found in the project dict

---
+++ lib/widgets/home/add_project.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:keeper_of_projects/backend/data.dart';
+import 'package:keeper_of_projects/data.dart';
+
+class AddProject extends StatefulWidget {
+  const AddProject({super.key});
+
+  @override
+  State<AddProject> createState() => _AddProjectState();
+}
+
+class _AddProjectState extends State<AddProject> with SingleTickerProviderStateMixin {
+  late final Animation<Offset> slideAnimation;
+  late final Animation<double> scaleAnimation;
+  late final Animation<double> fadeAnimation;
+  late AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      duration: Duration(milliseconds: 2000),
+      vsync: this,
+    )..repeat();
+    slideAnimation = TweenSequence<Offset>(
+      [
+        TweenSequenceItem<Offset>(
+            tween: Tween<Offset>(
+              begin: Offset.zero,
+              end: const Offset(0, -0.2),
+            ),
+            weight: 2),
+        TweenSequenceItem<Offset>(
+            tween: Tween<Offset>(
+              begin: const Offset(0, -0.2),
+              end: Offset.zero,
+            ).chain(
+              CurveTween(curve: Curves.linear),
+            ),
+            weight: 2),
+        TweenSequenceItem<Offset>(
+            tween: Tween<Offset>(
+              begin: Offset.zero,
+              end: const Offset(0, -0.2),
+            ),
+            weight: 2),
+        TweenSequenceItem<Offset>(
+            tween: Tween<Offset>(
+              begin: const Offset(0, -0.2),
+              end: Offset.zero,
+            ).chain(
+              CurveTween(curve: Curves.linear),
+            ),
+            weight: 2),
+        TweenSequenceItem<Offset>(
+            tween: Tween<Offset>(
+              begin: Offset.zero,
+              end: Offset.zero,
+            ).chain(
+              CurveTween(curve: Curves.linear),
+            ),
+            weight: 14),
+      ],
+    ).animate(animationController);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (userDataContent!["projects"].length == 0) {
+      animationController.repeat();
+    } else {
+      animationController.reset();
+      animationController.stop();
+    }
+    return SlideTransition(
+      position: slideAnimation,
+      child: FloatingActionButton(
+        elevation: 5,
+        backgroundColor: Pallete.primary,
+        onPressed: () {
+          print(true);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
