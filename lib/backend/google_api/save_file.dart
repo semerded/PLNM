0c6d8e1201026911ed63ddd313464224c52b9a8a
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2024-07-19 23:23:22 +0200

    added a methode to save (update) a file to google drive

---
+++ lib/backend/google_api/save_file.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:keeper_of_projects/backend/google_api/http_client.dart';
+import 'package:keeper_of_projects/data.dart';
+import "package:googleapis/drive/v3.dart" as drive;
+
+Future<void> saveFile(String fileId, String newContent) async {
+  final authHeaders = await currentUser!.authHeaders;
+  final authenticateClient = GoogleHttpClient(authHeaders);
+  final driveApi = drive.DriveApi(authenticateClient);
+
+  var media = drive.Media(
+    Stream.value(utf8.encode(newContent)),
+    utf8.encode(newContent).length,
+  );
+
+  try {
+    var response = await driveApi.files.update(
+      drive.File(),
+      fileId,
+      uploadMedia: media,
+    );
+    print('File updated: ${response.name} with ID: ${response.id}');
+  } catch (e) {
+    print('Error updating file: $e');
+  }
+}
