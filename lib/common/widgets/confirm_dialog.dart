31afcd543d82f2caf4916738cd2e90160963b654
Sem VB <sem.van.broekhoven@gmail.com>
2024-08-19 02:10:11 +0200

    created a confirm dialog

---
+++ lib/common/widgets/confirm_dialog.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:keeper_of_projects/common/widgets/text.dart';
+import 'package:keeper_of_projects/data.dart';
+
+Future<bool> showConfirmDialog(BuildContext context, String title) async {
+  return await showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        backgroundColor: Palette.topbox,
+        title: AdaptiveText(title),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context, false);
+            },
+            child: AdaptiveText("Cancel"),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context, true);
+            },
+            child: const Text(
+              "Confirm",
+              style: TextStyle(color: Palette.primary),
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
