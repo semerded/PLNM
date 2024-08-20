d6ede0297000153598bca76256a700e88d9159ff
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2024-08-20 15:28:44 +0200

    added the funcitionality to convert an idea into a project

--- lib/screens/projects/ideas_page.dart
+++ lib/screens/projects/ideas_page.dart
@@ -4,6 +4,8 @@ import 'package:keeper_of_projects/common/widgets/icon.dart';
 import 'package:keeper_of_projects/common/widgets/text.dart';
 import 'package:keeper_of_projects/data.dart';
 import 'package:keeper_of_projects/screens/projects/add_idea_page.dart';
+import 'package:keeper_of_projects/screens/projects/convert_idea_into_project_page.dart';
+import 'package:keeper_of_projects/screens/projects/edit_project_page.dart';
 
 class IdeasPage extends StatefulWidget {
   const IdeasPage({super.key});
@@ -15,61 +17,83 @@ class IdeasPage extends StatefulWidget {
 class _IdeasPageState extends State<IdeasPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Palette.bg,
-      appBar: AppBar(
-        title: Text("You have ${ideasContent.length} idea${ideasContent.length != 1 ? "s" : ""}"),
-        backgroundColor: Palette.primary,
-      ),
-      body: Expanded(
-        child: ListView.builder(
-          itemCount: ideasContent.length,
-          itemBuilder: (context, index) {
-            Map idea = ideasContent[index];
-            return Card(
-              color: Palette.box,
-              child: ListTile(
-                title: AdaptiveText(idea["title"]),
-                subtitle: AdaptiveText(idea["description"]),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: const AdaptiveIcon(Icons.lightbulb),
-                    ),
-                    IconButton(
-                      onPressed: () async {
-                        if (await showConfirmDialog(context, "Delete this idea")) {
-                          setState(() {
-                            ideasContent.removeAt(index);
-                          });
-                        }
-                      },
-                      icon: const AdaptiveIcon(Icons.delete),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.pop(context, true);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        backgroundColor: Palette.bg,
+        appBar: AppBar(
+          title: Text("You have ${ideasContent.length} idea${ideasContent.length != 1 ? "s" : ""}"),
+          backgroundColor: Palette.primary,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Palette.primary,
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute<bool>(
-              builder: (context) => const AddIdeaPage(),
-            ),
-          ).then((callback) {
-            if (callback != null && callback) {
-              setState(() {});
-            }
-          });
-        },
-        child: const Icon(Icons.add),
+        body: Expanded(
+          child: ListView.builder(
+            itemCount: ideasContent.length,
+            itemBuilder: (context, index) {
+              Map idea = ideasContent[index];
+              return Card(
+                color: Palette.box,
+                child: ListTile(
+                  title: AdaptiveText(idea["title"]),
+                  subtitle: AdaptiveText(idea["description"]),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute<Map>(
+                              builder: (context) => ConvertIdeaIntoProjectPage(
+                                idea: idea,
+                                ideaIndex: index,
+                              ),
+                            ),
+                          ).then((callback) {
+                            if (callback != null) {
+                              setState(() {
+                                projectsContent.add(callback);
+                              });
+                            }
+                          });
+                        },
+                        icon: const AdaptiveIcon(Icons.lightbulb),
+                      ),
+                      IconButton(
+                        onPressed: () async {
+                          if (await showConfirmDialog(context, "Delete this idea")) {
+                            setState(() {
+                              ideasContent.removeAt(index);
+                            });
+                          }
+                        },
+                        icon: const AdaptiveIcon(Icons.delete),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Palette.primary,
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute<bool>(
+                builder: (context) => const AddIdeaPage(),
+              ),
+            ).then((callback) {
+              if (callback != null && callback) {
+                setState(() {});
+              }
+            });
+          },
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
