a5215382651f2f0a822403455623ee67cc1d66a2
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2024-09-12 23:16:22 +0200

    fixed: when less than 2 projects are pressent, the home page wouldn't load

--- lib/screens/home/functions/search_most_progressed.dart
+++ lib/screens/home/functions/search_most_progressed.dart
@@ -6,6 +6,8 @@ import 'package:keeper_of_projects/common/functions/calculate_completion.dart';
 List searchMostProgressedProjects(int amount) {
   List sortedProjects = json.decode(json.encode(projectsDataContent!["projects"]));
   sortedProjects.sort((a, b) => (calculateCompletion(a["part"]) * 100).toInt().compareTo((calculateCompletion(b["part"]) * 100).toInt()));
-  sortedProjects.sublist(sortedProjects.length - amount, sortedProjects.length);
+  if (amount < sortedProjects.length) {
+    sortedProjects.sublist(sortedProjects.length - amount, sortedProjects.length);
+  }
   return sortedProjects.reversed.toList();
 }
--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/widgets.dart';
 import 'package:keeper_of_projects/backend/data.dart';
 import 'package:keeper_of_projects/common/functions/calculate_completion.dart';
+import 'package:keeper_of_projects/common/widgets/add_button_animated.dart';
 import 'package:keeper_of_projects/common/widgets/bottom_navigation_bar.dart';
 import 'package:keeper_of_projects/common/widgets/google_pop_up_menu.dart';
 import 'package:keeper_of_projects/common/widgets/icon.dart';
@@ -12,6 +13,7 @@ import 'package:keeper_of_projects/data.dart';
 import 'package:keeper_of_projects/screens/home/functions/search_most_progressed.dart';
 import 'package:keeper_of_projects/screens/home/widgets/home_page_title_divider.dart';
 import 'package:keeper_of_projects/screens/home/widgets/preview_tile.dart';
+import 'package:keeper_of_projects/screens/projects/add_project_page.dart';
 import 'package:keeper_of_projects/screens/projects/projects_page.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:keeper_of_projects/screens/projects/view_project_page.dart';
@@ -30,7 +32,6 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     // TODO: implement initState
     mostProgressedProjects = searchMostProgressedProjects(2);
-    print(mostProgressedProjects);
     super.initState();
   }
 
@@ -100,62 +101,107 @@ class _HomePageState extends State<HomePage> {
                   ],
                 ),
                 StaggeredGrid.count(
-                  crossAxisCount: 5,
-                  mainAxisSpacing: 4,
-                  crossAxisSpacing: 4,
-                  children: [
-                    StaggeredGridTile.count(
-                      crossAxisCellCount: 4,
-                      mainAxisCellCount: 1,
-                      child: PreviewTile(
-                        title: mostProgressedProjects[0]["title"],
-                        completion: calculateCompletion(mostProgressedProjects[0]["part"]),
-                        navigateToOnClick: ProjectViewPage(
-                          index: projectsContent.indexOf(mostProgressedProjects[0]),
-                          projectData: mostProgressedProjects[0],
-                        ),
-                      ),
-                    ),
-                    StaggeredGridTile.count(
-                      crossAxisCellCount: 4,
-                      mainAxisCellCount: 1,
-                      child: PreviewTile(
-                        navigateToOnClick: ProjectViewPage(
-                          index: projectsContent.indexOf(mostProgressedProjects[1]),
-                          projectData: mostProgressedProjects[1],
-                        ),
-                        title: mostProgressedProjects[1]["title"],
-                        completion: calculateCompletion(mostProgressedProjects[1]["part"]),
-                      ),
-                    ),
-                    StaggeredGridTile.count(
-                      crossAxisCellCount: 1,
-                      mainAxisCellCount: 2,
-                      child: IconButton(
-                        style: IconButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          backgroundColor: Palette.box,
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute<void>(
-                              builder: (context) {
-                                activePage = 4;
-                                return const ProjectPage();
-                              },
+                    crossAxisCount: 5,
+                    mainAxisSpacing: 4,
+                    crossAxisSpacing: 4,
+                    children: () {
+                      List<Widget> children = [];
+                      if (mostProgressedProjects.isNotEmpty) {
+                        children.add(
+                          StaggeredGridTile.count(
+                            crossAxisCellCount: 4,
+                            mainAxisCellCount: 1,
+                            child: PreviewTile(
+                              title: mostProgressedProjects[0]["title"],
+                              completion: calculateCompletion(mostProgressedProjects[0]["part"]),
+                              navigateToOnClick: ProjectViewPage(
+                                index: projectsContent.indexOf(mostProgressedProjects[0]),
+                                projectData: mostProgressedProjects[0],
+                              ),
                             ),
-                          ).then((value) {
-                            setState(() {});
-                          });
-                        },
-                        icon: AdaptiveIcon(Icons.arrow_forward_ios),
-                      ),
-                    )
-                  ],
-                ),
+                          ),
+                        );
+                      }
+
+                      if (mostProgressedProjects.length > 1) {
+                        children.add(
+                          StaggeredGridTile.count(
+                            crossAxisCellCount: 4,
+                            mainAxisCellCount: 1,
+                            child: PreviewTile(
+                              navigateToOnClick: ProjectViewPage(
+                                index: projectsContent.indexOf(mostProgressedProjects[1]),
+                                projectData: mostProgressedProjects[1],
+                              ),
+                              title: mostProgressedProjects[1]["title"],
+                              completion: calculateCompletion(mostProgressedProjects[1]["part"]),
+                            ),
+                          ),
+                        );
+                      }
+
+                      if (mostProgressedProjects.length < 2) {
+                        children.add(
+                          StaggeredGridTile.count(
+                            crossAxisCellCount: 4,
+                            mainAxisCellCount: mostProgressedProjects.isEmpty ? 2 : 1,
+                            child: Expanded(
+                              child: Card(
+                                margin: const EdgeInsets.all(0),
+                                color: Palette.box,
+                                child: Center(
+                                  child: ListTile(
+                                    title: AdaptiveText("Add more projects!"),
+                                    trailing: AddButtonAnimated(
+                                        taskCreated: (value) {
+                                          if (value) {
+                                            setState(() {
+                                              projectsContent = projectsDataContent!["projects"];
+                                              mostProgressedProjects = searchMostProgressedProjects(2);
+                                              // update screen when task is created
+                                            });
+                                          }
+                                        },
+                                        routTo: const AddProjectPage()),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      }
+
+                      children.add(
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 2,
+                          child: IconButton(
+                            style: IconButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              backgroundColor: Palette.box,
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute<void>(
+                                  builder: (context) {
+                                    activePage = 4;
+                                    return const ProjectPage();
+                                  },
+                                ),
+                              ).then((value) {
+                                setState(() {});
+                              });
+                            },
+                            icon: AdaptiveIcon(Icons.arrow_forward_ios),
+                          ),
+                        ),
+                      );
+
+                      return children;
+                    }()),
                 HomePageTitleDivider(
                   title: "test",
                   titleDividerInfo: [],
