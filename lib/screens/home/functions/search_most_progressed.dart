c31a208f1d3e3c7e1d28cfba9f9801ef4f35b251
Sem VB <sem.van.broekhoven@gmail.com>
2024-09-16 14:44:52 +0200

    fixed: search most progressed function now filters progresses that are equal to 100%

--- lib/screens/home/functions/search_most_progressed.dart
+++ lib/screens/home/functions/search_most_progressed.dart
@@ -6,6 +6,9 @@ import 'package:keeper_of_projects/common/functions/calculate_completion.dart';
 List searchMostProgressedProjects(int amount) {
   List sortedProjects = json.decode(json.encode(projectsDataContent!["projects"]));
   sortedProjects.sort((a, b) => (calculateCompletion(a["part"]) * 100).toInt().compareTo((calculateCompletion(b["part"]) * 100).toInt()));
+
+  sortedProjects.removeWhere((a) => (calculateCompletion(a["part"]) * 100).toInt() == 100);
+
   if (amount < sortedProjects.length) {
     sortedProjects.sublist(sortedProjects.length - amount, sortedProjects.length);
   }
