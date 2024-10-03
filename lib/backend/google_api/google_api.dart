c7b758e9e209d4669bad37df9ceda2fc1ceb0298
Sem VB <sem.van.broekhoven@gmail.com>
2024-10-03 10:44:32 +0200

    fixed: after login out you will directly be logged in again

--- lib/backend/google_api/google_api.dart
+++ lib/backend/google_api/google_api.dart
@@ -1,14 +1,21 @@
+import "dart:io";
+
+import "package:keeper_of_projects/backend/google_api/desktop_token_storage.dart";
 import "package:keeper_of_projects/data.dart";
 
 Future<void> handleSignIn() async {
-    try {
-      await googleSignIn.signIn();
-    } catch (error) {
-      print(error);
-    }
+  try {
+    await googleSignIn.signIn();
+  } catch (error) {
+    print(error);
   }
+}
 
-  Future<void> handleSignOut() async {
+Future<void> handleSignOut() async {
+  if (Platform.isAndroid || Platform.isIOS) {
     await googleSignIn.disconnect();
-    currentUser = null;
+  } else {
+    logout();
   }
+  currentUser = null;
+}
